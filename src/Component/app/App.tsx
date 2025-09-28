36dfc74e50503b6a11791de4e714d72bba746212
MaksPzm <makspzm@gmail.com>
2025-09-28 23:34:26 +0500

    kanban

---
+++ src/Component/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from "../layout/Layout";
+import './App.scss';
+
+function App(): React.JSX.Element {
+
+  return (
+    <div className="App">
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
