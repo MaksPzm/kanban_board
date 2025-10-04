14529f910506ef88bdc83ef72272a343da2da8b5
MaksPzm <makspzm@gmail.com>
2025-10-04 22:52:12 +0500

    Небоьшие доработки "Ready"

--- src/Component/main/Main.tsx
+++ src/Component/main/Main.tsx
@@ -1,13 +1,85 @@
-import React from "react";
+import React, {MouseEventHandler, useState} from "react";
 import TaskBlock from "../taskBlock/TaskBlock";
+import styles from "../taskBlock/taskBlock.module.scss";
+
+type Task = {id: number, name: string, description: string, task: string};
 
 export default function Main(): React.JSX.Element {
+    const [task, setTask] = useState<[Task] | []>([]);
+    const changeTask = (change: Task) => {
+        if (task == undefined) return;
+        // @ts-ignore
+        setTask([...task, change]);
+    }
+    console.log("task", task);
     return (
         <div className="main">
-            <TaskBlock task={"Backlog"}/>
-            <TaskBlock task={"Ready"}/>
-            <TaskBlock task={"In Progress"}/>
-            <TaskBlock task={"Finished"}/>
+            <TaskBlock task={"Backlog"} name={"Backlog"} changeTask={changeTask} showBtnSubmit={true}
+                children={task.map((value: Task, index: number) =>
+                   <li key={index} id={`${value.id}`} className={styles.taskBlock__section_list_item} onClick={(event: React.MouseEvent) => {
+                       console.log(event)
+                   }}>
+                       {value.name}
+                   </li>)
+                }
+            />
+            <TaskBlock task={"Ready"} name={"Ready"} showBtnSubmit={false}
+                       children={task.map((value: Task, index: number) =>
+                           <li key={index} id={`${value.id}`} className={styles.taskBlock__section_list_item_ready} onClick={(event: React.MouseEvent) => {
+                               console.log(event)
+                           }}>
+                               {value.name}
+                           </li>)
+                       }
+            />
         </div>
-    );
+    )
 }
+
+
+
+
+
+
+// import TaskBlock from "../taskBlock/TaskBlock";
+//
+// type context = [{task: string, data: [{id: string, name: string, description: string}]}];
+//
+// const defaultValue: context = [
+//     {
+//         "task": "",
+//         "data": [
+//             {
+//                 "id": "",
+//                 "name": "",
+//                 "description": ""
+//             }
+//         ]
+//     }
+// ];
+//
+// export const MainContext = createContext<context>(defaultValue);
+// export default function Main(): React.JSX.Element {
+//     const [data, setData] = useState<context>(defaultValue);
+//     // const [taskBacklog, setTaskBacklog] = useState<context>(defaultValue);
+//     // const [taskReady, setTaskReady] = useState<context>(defaultValue);
+//     // const [progress, setProgress] = useState<context>(defaultValue);
+//     // const [finished, setFinished] = useState<context>(defaultValue);
+//     // const sectionBacklog = useCallback((dataBacklog: context) => {
+//     // setTaskBacklog(dataBacklog);
+//     // }, [setTaskBacklog]);
+//     const dataTask = useCallback((form: any) => {
+//     setData({...data}, form);
+//     }, []);
+//     return (
+//         <MainContext.Provider value={data}>
+//             <div className="main">
+//                 <TaskBlock dataTask={dataTask} task={"Backlog"} id={"Backlog"}/>
+//                 <TaskBlock dataTask={dataTask} task={"Ready"} id={"Ready"}/>
+//                 <TaskBlock dataTask={dataTask} task={"In Progress"} id={"InProgress"}/>
+//                 <TaskBlock dataTask={dataTask} task={"Finished"} id={"Finished"}/>
+//             </div>
+//         </MainContext.Provider>
+//     );
+// }
+
--- src/Component/taskBlock/TaskBlock.tsx
+++ src/Component/taskBlock/TaskBlock.tsx
@@ -1,27 +1,53 @@
-import React from "react";
-import styles from "./taskBlock.module.scss"
-
-interface ContainerProps {
+import React, {useCallback, useState} from "react";
+import styles from "./taskBlock.module.scss";
+interface ITask {
     task: string;
+    name: string;
+    showBtnSubmit: boolean;
+    children?: React.ReactNode;
+    changeTask?: Function | undefined;
 }
-export default function TaskBlock({task}: ContainerProps): React.JSX.Element {
-    return (
-        <div className={styles.taskBlock}>
+
+export default function TaskBlock({task, name, showBtnSubmit, children, changeTask}: ITask) {
+    const [showSubmitBtn, setShowSubmitBtn] = useState<boolean>(false);
+    const [change, setChange] = useState<string>("");
+    const [results, setResults] = useState<{}>({});
+    const [id, setId] = useState<number>(0);
+    const [showList, setShowList] = useState<boolean>(showBtnSubmit);
+    const [showDropList, setShowDropList] = useState(true);
+    const clickBtn = useCallback(() => {
+        setShowSubmitBtn(true);
+    }, []);
+    const pressKey = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setChange(e.currentTarget.value)
+    }
+    const clickSubmit = useCallback((e: React.MouseEvent) => {
+        e.preventDefault();
+        setShowSubmitBtn(false);
+        setId(id => id + 1);
+        type result = {id: number, name: string, description: string, task: string};
+        let res: result = {id: id, name: change, description: "This task has no description", task: "backlog"};
+        if (changeTask !== undefined) changeTask(res);
+        setResults(res);
+    }, [change, id])
+    console.log(results)
+    return  (
+        <div className={styles.taskBlock} id={task}>
             <div className={styles.taskBlock__section}>
-                <h1 className={styles.taskBlock__section_title}>{task}</h1>
+                <h1 className={styles.taskBlock__section_title}>{name}</h1>
                 <ul className={styles.taskBlock__section_list}>
-                    <li className={styles.taskBlock__section_list_item}>
-                        <p className={styles.taskBlock__section_list_item_text}>fgdfgfdg</p>
-                    </li>
-                    <li className={styles.taskBlock__section_list_item}>
-                        <p className={styles.taskBlock__section_list_item_text}>fgdfgfgghjgjgjghjgjhsd ggfsdfsdfsggggghfghfdg</p>
-                    </li>
-                    <button className={styles.taskBlock__section_list_btn}>
-                        <span className={styles.taskBlock__section_list_btn_sp}>+</span> Add card
-                    </button>
+                    {showList && children}
+                    {showSubmitBtn ? (showList ? <input id="taskIn" type="text" className={styles.taskBlock__section_list_input} onChange={pressKey} placeholder="________________________"/>
+                            : <div className={styles.section__list} onClick={() => setShowDropList(showDropList => !showDropList) }></div>)
+                    : ""}
+                    {showSubmitBtn
+                        ? (showList ? <button type="submit" className={styles.taskBlock__section_list_btn} onClick={clickSubmit}>Submit</button>
+                            : (showDropList ? <div className={styles.dropList}>{children}</div> : "")  )
+                        : <button type="button" className={styles.taskBlock__section_list_btn} onClick={clickBtn}>
+                            <span className={styles.taskBlock__section_list_btn_sp}>+</span> Add card
+                        </button>}
                 </ul>
             </div>
-
         </div>
-    );
+    )
 }
